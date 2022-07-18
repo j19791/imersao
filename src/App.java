c00fabe88f479461e95132148fab52d30b1bbf20
Jefferson <jeffrm@gmail.com>
2022-07-18 19:55:26 -0300

    AULA 01 - CONSUMINDO UMA API DE FILMES COM JAVA

--- src/App.java
+++ src/App.java
@@ -2,22 +2,46 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        var url = "https://imdb-api.com/en/API/Top250Movies/k_jhl7ya19";
+        //fazer uma conexão http: request/response
+
+        //uri é o mais genérico
+        var url = "https://api.mocki.io/v2/549a5d8b"; //a api do imdb aqui - usando uma alternativa
 
         var uri = URI.create(url); //URI contem uma url
 
-        var client = HttpClient.newHttpClient();
+        //o java.net ja é a lib padrão p/ http
 
-        var request = HttpRequest.newBuilder(uri).build();
+        var client = HttpClient.newHttpClient(); //criar novo cliente
+       
+        var request = HttpRequest.newBuilder(uri).GET().build(); //criando o request
 
-        var json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        var json = client.send(request, HttpResponse.BodyHandlers.ofString()); //response
         
+        //extrair só os dados que interessam (parsear) - utilizando expressões regulares -regex - Jackson
+
+        JsonParser parser = new JsonParser();
+
+        List<Map<String, String>> listaDeFilmes = parser.parse(json.body());
+
+        System.out.println(listaDeFilmes.size());
+System.out.println(listaDeFilmes.get(0));
+
+for (Map<String,String> filme : listaDeFilmes) {
+    System.out.println(filme.get("title"));
+    System.out.println(filme.get("image"));
+    System.out.println(filme.get("imDbRating"));
+    System.out.println();
+}
+        //exibir e manipular os dados
         
-        
-        System.out.println(json);
+      //  System.out.println(listaDeFilmes); //sout
     }
 }
